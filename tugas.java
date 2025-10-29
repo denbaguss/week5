5e4f0b4236e08c3f9726c85ef81c97771a9bbae8
denbaguss <awanhexom@gmail.com>
2025-10-29 14:00:14 +0700

    tugas

--- tugas.java
+++ tugas.java
@@ -6,7 +6,23 @@ public class tugas {
     public static void main(String[] args) {
         double alas, tinggi, luas, keliling, sisi;
 
-        Scanner Scan= new Scanner(System.in)
+        Scanner Scan= new Scanner(System.in);
+
+        System.out.println("masukkan panjang alas segitiga");
+        alas=Scan.nextDouble();
+
+        System.out.println("massukan panjang keliling segitiga");
+        tinggi=Scan.nextDouble();
+
+        System.out.println("masukkan pangjang sisi segitiga sama sisi");
+        sisi=Scan.nextDouble();
+
+
+        luas = 0.5*alas*tinggi;
+        keliling = sisi+sisi+sisi;
+
+        System.out.println("nilai luas :" +luas);
+        System.out.println("nilai keliling :" +keliling);
         
     }
     
