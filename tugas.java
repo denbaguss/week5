9dd7a6a8dffbef8b04ec27e4f80b41b432978f63
denbaguss <awanhexom@gmail.com>
2025-10-29 13:46:17 +0700

    first commit

---
+++ tugas.java
@@ -0,0 +1,14 @@
+package week_5;
+
+import java.util.Scanner;
+
+public class tugas {
+    public static void main(String[] args) {
+        double alas, tinggi, luas, keliling, sisi;
+
+        Scanner Scan= new Scanner(System.in)
+        
+    }
+    
+
+}
